520fc2ef2bae8819eca008be828c8c83e5e2026f
Saim Raza <saimorsaim@gmail.com>
2022-04-10 18:19:05 -0400

    Custom module myName.js

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = myName('Saim');
     return element;
 }
 
