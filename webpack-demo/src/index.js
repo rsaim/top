000d01e51cb179a5ed3b252503a86926873b76ad
Saim Raza <saimorsaim@gmail.com>
2022-04-10 19:14:25 -0400

    Webpack: code to add css and images

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
+import YourAmazingIcon from './you-are-amazing.png';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = myName('Saim');
+    // Add css
+    element.classList.add('hello');
+    // Add image
+    const myIcon = new Image();
+    myIcon.src = YourAmazingIcon;
+    element.appendChild(myIcon);
+
     return element;
 }
 
