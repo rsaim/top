38f7783d62d8ce62c62ba40d49492e8dd75f8f3a
Saim Raza <saimorsaim@gmail.com>
2022-04-11 07:20:47 -0400

    Add a bit more styling

--- restaurant/webpack.config.js
+++ restaurant/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Development',
+            title: 'Dynamic JS Ichiraku',
         }),
     ],
     output: {
