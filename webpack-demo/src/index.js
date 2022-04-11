e7d9c2adc0897b7ac428e1c262805bfaccc24417
Saim Raza <saimorsaim@gmail.com>
2022-04-10 21:48:02 -0400

    html-webpack-plugin

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,9 +2,12 @@ import _ from 'lodash';
 import myName from './myName';
 import './style.css';
 import YourAmazingIcon from './you-are-amazing.png';
+import { printMe } from './print';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
+
     element.innerHTML = myName('Saim');
     // Add css
     element.classList.add('hello');
@@ -12,6 +15,10 @@ function component() {
     const myIcon = new Image();
     myIcon.src = YourAmazingIcon;
     element.appendChild(myIcon);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
---
+++ webpack-demo/src/print.js
@@ -0,0 +1,7 @@
+const printMe = () => {
+    console.log("I get called from print.js!!");
+}
+
+export {
+    printMe,
+};
