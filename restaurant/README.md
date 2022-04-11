832bbe2a667811a3624b310632bb2611b3112632
Saim Raza <saimorsaim@gmail.com>
2022-04-11 05:46:20 -0400

    restaurant site with dynamic JS

---
+++ restaurant/README.md
@@ -0,0 +1,76 @@
+- $ mkdir src
+
+- $ touch src/index.js
+
+- copy the following config
+
+```json
+{
+  "name": "webpack-demo",
+  "version": "1.0.0",
+  "description": "",
+  "private": "true",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "build": "webpack",
+    "start": "webpack serve --open"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "css-loader": "^6.7.1",
+    "html-webpack-plugin": "^5.5.0",
+    "style-loader": "^3.3.1",
+    "webpack": "^5.72.0",
+    "webpack-cli": "^4.9.2"
+  },
+  "dependencies": {
+    "lodash": "^4.17.21",
+    "webpack-dev-server": "^4.8.1"
+  }
+}
+```
+
+- Copy the following `webpack.config.js`
+```json
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: "development",
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
+```
+
+- $ npm install
+
+- $ npm run build
+
+- $ npm start
---
+++ restaurant/src/index.js
@@ -0,0 +1,62 @@
+import './style.css';
+// import RamenIchirakuCover from './Ramen-IchirakuCover.jpeg';
+import Naruto from './Naruto.jpeg';
+
+function createHeader() {
+    const header = document.createElement('div');
+    header.setAttribute("class", "header");
+    // Heading
+    const h1 = document.createElement('h1');
+    h1.innerText = "Ichiraku Ramen";
+    header.append(h1);
+    // Navbar
+    const navBar = document.createElement("nav");
+    const homeButton = document.createElement('button');
+    homeButton.innerText = 'Home';
+    navBar.appendChild(homeButton);
+
+    const menuButton = document.createElement('button');
+    menuButton.innerText = 'Menu';
+    navBar.appendChild(menuButton);
+
+    const contactButton = document.createElement('button');
+    contactButton.innerText = 'Contact';
+    navBar.appendChild(contactButton);
+
+    header.appendChild(navBar);
+
+    return header;
+}
+
+function createMain() {
+    const main = document.createElement('div');
+    main.setAttribute("class", "main");
+    
+    // Add image
+    const narutoImg = new Image();
+    narutoImg.src = Naruto;
+    narutoImg.setAttribute("id", "naruto");
+    main.appendChild(narutoImg);
+    
+    return main;
+}
+
+function createFooter() {
+    const footer = document.createElement('div');
+    footer.setAttribute("class", "footer");
+    return footer;
+}
+
+function renderHomePage() {
+    const content = document.createElement('div');
+    content.setAttribute("class", "content");
+    
+    content.appendChild(createHeader());
+    content.appendChild(createMain());
+    content.appendChild(createFooter());
+
+    document.body.appendChild(content);
+
+}
+
+renderHomePage();
